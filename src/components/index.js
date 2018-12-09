b354cdf869d0a51d82514b0f45d76b8ce2cbd95d
Bruno Vukasovic <vukasovic.bruno@gmail.com>
2018-12-09 18:00:12 +0100

    TripItem component added

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as Container } from "./Container";
 export { default as Layout } from "./Layout";
 export { default as BlogIndex } from "./BlogIndex";
 export { default as MainImage } from "./MainImage";
+export { default as TripItem } from "./TripItem";
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { graphql } from "gatsby";
-import { Layout } from "../components";
-import { MainImage } from "../components";
-import mainPhoto  from "../images/plitvice.jpg";
+import { Layout, Container, MainImage, TripItem } from "../components";
 import krkaPhoto from "../images/krka 4x3.jpg";
-
+import zipPhoto from "../images/zip 4x3.jpg";
+import blueCavePhoto from "../images/blue cave 4x3.jpg";
+import raftingPhoto from "../images/rafting 4x3.jpg";
 
 
 export default ({
@@ -17,7 +17,27 @@ export default ({
   return (
     <Layout>
       <MainImage />
-      <img src={krkaPhoto} alt="Krka" class="item-image" width="50%" />
+
+      <Container>
+        <TripItem>
+        <img src={krkaPhoto} alt="Krka" width= "100%"/>
+        <p>Krka Waterfalls</p>
+        </TripItem>
+        <TripItem>
+        <img src={zipPhoto} alt="Zip line" width= "100%"/>
+        <p>Zip line</p>
+        </TripItem>
+        <TripItem>
+        <img src={blueCavePhoto} alt="Blue Cave" width= "100%"/>
+        <p>Blue Cave</p>
+        </TripItem>
+        <TripItem>
+        <img src={raftingPhoto} alt="Rafting" width= "100%"/>
+        <p>Rafting</p>
+        </TripItem>
+      </Container>
+      
+      <h1>{title}</h1>
       <h1>{title}</h1>
       <p>{description}</p>
     </Layout>
